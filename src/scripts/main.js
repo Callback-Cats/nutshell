463ec5c10532a6f576326a9cb530975dd593685c
Erin Polley <erinepolley@gmail.com>
2019-11-13 15:26:46 -0600

    rendered data to DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,12 @@
 import taskDomRenderers from "./tasks/domRenderer.js"
-import taskEventListeners from "./tasks/eventListeners.js";
 import API from "./tasks/data.js";
 console.log("We got this, guys.")
 // sessionStorage.setItem("activeUser", user.id)
 
 taskDomRenderers.taskFormRender()
 API.getTaskData()
+    .then(tasks => tasks.forEach(task => {
+        console.log(task)
+        taskDomRenderers.taskRender(task)
+    })
+    )
