64495c1e67a6c3c3fb3232b48e76759448f07cf8
Matthew Blagg <matthewscottblagg@gmail.com>
2019-11-13 11:10:36 -0600

    beginning messages

--- src/scripts/messages/data.js
+++ src/scripts/messages/data.js
@@ -1,28 +1,9 @@
-{
-    "messages": [
-        {
-            "id": 1,
-            "user_id": 1,
-            "message": "Hey, what is up?",
-            "time_stamp": "11.11.2019 8:00:02"
-        },
-        {
-            "id": 2,
-            "user_id": 2,
-            "message": "Hello, this message is for you...",
-            "time_stamp": "11.11.2019 8:00:02"
-        },
-        {
-            "id": 3,
-            "user_id": 3,
-            "message": "I do not acknowledge your message.",
-            "time_stamp": "11.11.2019 8:00:02"
-        },
-        {
-            "id": 4,
-            "user_id": 4,
-            "message": "I need help?",
-            "time_stamp": "11.11.2019 8:00:02"
-        }
-    ]
+const baseUrl = "http://localhost:8088/messages"
+
+const messageAPI = {
+    getMessageData: () => fetch(baseUrl)
+    .then(response => response.json()),
+    
 }
+
+export default API
